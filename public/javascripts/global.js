4bb2a20ebefa11d9590aaf768586d59d4f1d8430
Lucas Klaassen <lucas@vin65.com>
2014-03-10 10:30:16 -0700

    Cleaned up code and formatted structure

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,22 +1,12 @@
-// TODO MODEL
-// function toDoItem(id, input){
-// 	this.id = id;
-// 	this.input = input;
-// 	this.isComplete = false;
-// };
-
 var toDoController = {
 
 	refreshView : function() {
-
 		//EMPTY TODO LISTS
 		$('.activeToDos').html("");
 		var activeToDos = '';
 		$('.completedToDos').html("");
 		var completedToDos = '';
-
 		$.getJSON( '/todolist', function( data ) {
-
 		// For each todo in our JSON, add a checkbox and label containing the input todo string
 			$.each(data, function(){
 					if(this.isComplete == "false"){
@@ -33,13 +23,10 @@ var toDoController = {
 						completedToDos += '</div>';
 					}
 			});
-			
 			//Inject the todo strings into their existing HTML lists
 			$('.activeToDos').html(activeToDos);
 			$('.completedToDos').html(completedToDos);
-
 			//init event functions
-			
 			//Delete Todo
 			$('a.delete').on('click', toDoController.deleteToDoItem);
 			//Toggle Todo
@@ -64,10 +51,8 @@ var toDoController = {
 	},
 
 	createToDoItem : function(errorCount,input) {	
-
 			// Check and make sure errorCount's still at zero
 			if(errorCount === 0) {
-
 			  // Use AJAX to post the object to our adduser service
 			  $.ajax({
 			    type: 'POST',
@@ -75,22 +60,16 @@ var toDoController = {
 			    url: '/addtodo',
 			    dataType: 'JSON'
 			  }).done(function( response ) {
-
 			    // Check for successful (blank) response
 			    if (response.msg === '') {
-
 			      // Clear the form inputs
 			      $('.inputToDo').val('');
-
 			      // Update the table
 			      toDoController.refreshView();
-
 			    }
 			    else {
-
 			      // If something goes wrong, alert the error message that our service returned
 			      alert('Error: ' + response.msg);
-
 			    }
 			  });
 			}
@@ -113,7 +92,6 @@ var toDoController = {
 			  		'isComplete': "false"
 			  	}
 			  }
-
 			  // Use AJAX to post the object to our adduser service
 			  $.ajax({
 			    type: 'PUT',
@@ -121,25 +99,19 @@ var toDoController = {
 			    url: '/toggletodo/' + $(this).attr('id'),
 			    dataType: 'JSON'
 			  }).done(function( response ) {
-
 			    // Check for successful (blank) response
 			    if (response.msg === '') {
-
 			      // Update the table
 			      toDoController.refreshView();
-
 			    }
 			    else {
-
 			      // If something goes wrong, alert the error message that our service returned
 			      alert('Error: ' + response.msg);
-
 			    }
 			  });
 	},
 
 	updateToDoItem : function(data, id){
-
 			  // Use AJAX to post the object to our adduser service
 			  $.ajax({
 			    type: 'PUT',
@@ -147,147 +119,55 @@ var toDoController = {
 			    url: '/updatetodo/' + id,
 			    dataType: 'JSON'
 			  }).done(function( response ) {
-
 			    // Check for successful (blank) response
 			    if (response.msg === '') {
-
 			      // Update the table
 			      toDoController.refreshView();
-
 			    }
 			    else {
-
 			      // If something goes wrong, alert the error message that our service returned
 			      alert('Error: ' + response.msg);
-
 			    }
 			  });
 	},
 
 	deleteToDoItem : function(event){
 	  event.preventDefault();
-
 	  // Pop up a confirmation dialog
 	  var confirmation = confirm('Are you sure you want to delete this todo?');
-
 	  // Check and make sure the user confirmed
 	  if (confirmation === true) {
-
-	  	console.log($(this));
-
 	    // If they did, do our delete
 	    $.ajax({
 	      type: 'DELETE',
 	      url: '/deletetodo/' + $(this).attr('id')
 	    }).done(function( response ) {
-
 	      // Check for a successful (blank) response
 	      if (response.msg === '') {
 	      }
 	      else {
 	        alert('Error: ' + response.msg);
 	      }
-
 	      // Update the table
 	      toDoController.refreshView();
-
 	    });
-
 	  }
 	  else {
-
 	    // If they said no to the confirm, do nothing
 	    return false;
-
 	  }
 	}
-
-	// totalTodos: 0,
-
-	// toDoList: {},
-
-	// init : {
-
-	// 	// createToDoItemDisplay : function() {
-	// 	// 	$('.inputToDo').on("keyup", function(e){
-	// 	// 		if(e.keyCode == 13){
-	// 	// 			var input = $('.inputToDo').val();
-	// 	// 			toDoController.createToDoItem(input);
-	// 	// 			$('.inputToDo').val('');
-	// 	// 		}
-	// 	// 	});
-	// 	// },
-
-	// 	// toggleToDoItemDisplay : function() {
-	// 	// 	$(document).on("change", ".checkbox input[type=checkbox]", function(){
-	// 	// 		toDoController.toggleToDoItem($(this).attr('id'));
-	// 	// 	});
-	// 	// },
-
-	// 	// deleteToDoItemDisplay : function() {
-	// 	// 	$(document).on("click", "a.delete", function(){
-	// 	// 		toDoController.deleteToDoItem($(this).attr("id"));
-	// 	// 	});
-	// 	// },
-
-	// 	// updateToDoItemDisplay : function() {
-	// 	// 	$(document).on("click", "label.toDoInput", function() {
-	// 	// 			var prevContent = $(this).html();
-	// 	// 			$(this).replaceWith("<input type=\"text\" value=\"" + prevContent + "\" class=\"editToDoInput\"  />");
-	// 	// 			$('.editToDoInput').focus();
-	// 	// 			$('.editToDoInput').on("keyup", function(e){
-	// 	// 				if(e.keyCode == 13){
-	// 	// 					var input = $('.editToDoInput').val();
-	// 	// 					var id = $(this).prev().attr("id");
-	// 	// 					toDoController.updateToDoItem(id, input);
-	// 	// 					if(input === ""){
-	// 	// 						$(this).replaceWith('<label class="toDoInput">' + prevContent + '</label>');
-	// 	// 					} else {
-	// 	// 						$(this).replaceWith('<label class="toDoInput">' + input + '</label>');
-	// 	// 					}
-	// 	// 				}
-	// 	// 			});
-	// 	// 	});	
-	// 	// }
-
-	// },
-
-	// toggleToDoItem : function(id){
-	// 	this.toDoList[id].isComplete = !this.toDoList[id].isComplete;
-	// 	this.refreshView();
-	// },
-
-	// deleteToDoItem : function(id){
-	// 	delete this.toDoList[id];
-	// 	this.refreshView();
-	// },
-
-	// updateToDoItem : function(id, input) {
-	// 	if(input === ""){
-	// 		alert("Yeah right buddy! Type something.")
-	// 		return this.toDoList[id].input = this.toDoList[id].input;
-	// 	} else {
-	// 		return this.toDoList[id].input = input;
-	// 	}
-	// }
 
 };
 
 $(document).ready(function(){
-	// toDoController.init.createToDoItemDisplay();
-	// toDoController.init.toggleToDoItemDisplay();
-	// toDoController.init.updateToDoItemDisplay();
-	// toDoController.init.deleteToDoItemDisplay();
 	toDoController.refreshView();
 	$('.inputToDo').on("keyup", function(e){
 		if(e.keyCode == 13){
-
 			var errorCount = 0;
-
 			if($('.inputToDo').val() === ''){
 				errorCount++;
 			};
-
 			var inputData = {
 				'input': $(this).val(),
 				'isComplete': "false"
